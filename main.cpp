102839502c2bb0b7740a448738832fb7d33ee9f0
dathellavader <ella.henner@pcc.edu>
2025-11-08 03:11:43 +0000

    waaaa

--- main.cpp
+++ main.cpp
@@ -38,12 +38,69 @@ using namespace std;
 
 int main(int argc, char* argv[]) {
   
+  cout << "Welcome to my Coffee/Tea Vending Machine!" << endl;
+
+  bool again = true;
+  float total_USD = 0.0;
+  float balance_USD = 0.0;
+  int coin = 0;
+
+    while(again) {
+
+    do {
+    cout << "Enter coins - 5, 10, or 25 only: ";
+    cin >> coin;
+while (coin != 5 && coin != 10 && coin != 25 && coin != 0) {
+    cout << endl << "Invalid!";
+    break;
+}
+while(coin != 0) {
+    again = true;
+}
+    // Your balance is $0.30
+
+    // Please pick an option ($0.25 each):
+    //     C/c: Coffee
+    //     T/t: Tea
+    //     Q/q: Quit
+    // >> c
+
+
+    // How many would you like?
+    // >> 2
+
+    // Your total is $0.50
+    // Your balance is $0.30
+    if()
+
+    // Not enough change!! Please add more coins.
+  }
 
 
 
 
 
 
+// Enter coins - 5, 10, or 25 only: 5
+// Enter coins - 5, 10, or 25 only: 25
+// Enter coins - 5, 10, or 25 only: 0
+
+// Your balance: $0.60
+
+// Please pick an option ($0.25 each):
+//     C/c: Coffee
+//     T/t: Tea
+//     Q/q: Quit
+// >> T
+
+// How many would you like?
+// >> 1
+
+// Your total is $0.25
+// Your balance is $0.35
+
+  cout << "Thank you for using my Vending Machine Program!" << endl;
+}
 
 
   return 0;
